d75d450385b2bf4ffbc6681b97fe99ed4b048b0d
MAriana Olivas <olivasgarcia031096@gmail.com>
2022-10-05 10:29:49 -0500

    carpeta

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,6 +75,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    publicPath: 'desarrollos/',
     postcss: {
       plugins: {
         tailwindcss: {
